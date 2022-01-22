a48f138c0e94e89d6a143ddb947e54bd252814d7
Cooper German <yannickleprovost@wanadoo.fr>
2022-01-22 01:07:40 +0100

    [`DEPLOYMENT`] : 2022-01-22 release

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "0d1c670427af988b01460c066ae3a907",
+  "main.dart.js": "f86d542a3fc7377cf069f4f03fd6d262",
 "version.json": "fc609ec2168bd04bb9ae41924c0403cb",
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
@@ -21,8 +21,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/AssetManifest.json": "63ca4806e390faab9bca9ba85f61ac83",
-"index.html": "e22ee0ed26cb31f051b1f9e88f20a503",
-"/": "e22ee0ed26cb31f051b1f9e88f20a503",
+"index.html": "2c5cf66460805e42544fd408742121a3",
+"/": "2c5cf66460805e42544fd408742121a3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "d8d86055f396da577c3809e06ff975d6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
